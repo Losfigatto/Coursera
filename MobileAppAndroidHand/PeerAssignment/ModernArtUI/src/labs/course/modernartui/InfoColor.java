ee75c2724da131cc89d6e985f4cadb704d7a2314
losfigatto <losfigatto@gmail.com>
2014-10-25 15:56:49 +0200

    PeerAssignment Modern Art UI

--- MobileAppAndroidHand/PeerAssignment/ModernArtUI/src/labs/course/modernartui/InfoColor.java
+++ MobileAppAndroidHand/PeerAssignment/ModernArtUI/src/labs/course/modernartui/InfoColor.java
@@ -4,7 +4,45 @@ import android.graphics.Color;
 
 public class InfoColor {
 	
-	public InfoColor(Color start,Color end){
+	private int startGreen;
+	private int startBlue;
+	private int startRed;
+	
+	private int endGreen;
+	private int endBlue;
+	private int endRed;
+	
+	private int ratioGreen;
+	private int ratioBlue;
+	private int ratioRed;
+	
+	public InfoColor(int startColor,int endColor){
+		startGreen = Color.green(startColor);
+		startBlue = Color.blue(startColor);
+		startRed = Color.red(startColor);
 		
+		endGreen = Color.green(endColor);
+		endBlue = Color.blue(endColor);
+		endRed = Color.red(endColor);
+		ratioGreen = 0;
+		ratioBlue = 0;
+		ratioRed = 0 ;
+	}
+	
+	public void setMaxTransition(int maxTransition){
+		ratioRed = (int)Math.abs((endRed - startRed)/maxTransition);
+		ratioGreen = (int)Math.abs((endGreen - startGreen)/maxTransition);
+		ratioBlue = (int)Math.abs((endBlue - startBlue)/maxTransition);
+	}
+	
+	public int getNextColorFromRatio(int percent){
+		int red = startRed + ratioRed * percent;
+		int green = startGreen + ratioGreen * percent;
+		int blue = startBlue + ratioBlue * percent;
+		return Color.rgb(red, green, blue);
+	}
+	
+	public int getStartColor(){
+		return Color.rgb(startRed, startGreen, startBlue);
 	}
 }
--- MobileAppAndroidHand/PeerAssignment/ModernArtUI/src/labs/course/modernartui/MainActivity.java
+++ MobileAppAndroidHand/PeerAssignment/ModernArtUI/src/labs/course/modernartui/MainActivity.java
@@ -5,51 +5,98 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.drawable.ColorDrawable;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.app.DialogFragment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+
 /**
- * SEE http://stackoverflow.com/questions/19841477/java-smooth-color-transition
+ * 
  * @author amarasco
- *
+ * 
  */
+@SuppressLint("UseSparseArrays")
 public class MainActivity extends Activity {
 
-	public static List<Integer> elencoId = Arrays.asList(R.id.elem1,R.id.elem2,R.id.elem3,R.id.elem4);//,R.id.elem5,R.id.elem6,R.id.elem7,R.id.elem8);
+	public static List<Integer> elencoId = Arrays.asList(R.id.elem1,
+			R.id.elem2, R.id.elem3, R.id.elem4, R.id.elem5, R.id.elem6,
+			R.id.elem7, R.id.elem8);
 	
-	public static Map<Integer,InfoColor> transitionColor = new HashMap<Integer,InfoColor>();
+	static private final String URL = "http://www.moma.com";
 	
+	@SuppressWarnings("serial")
+	public Map<Integer, InfoColor> transitionColor = new HashMap<Integer, InfoColor>() {
+		{
+			put(R.id.elem1,
+					new InfoColor(Color.rgb(255, 204, 0), Color.rgb(255, 204,
+							204)));
+			put(R.id.elem2,
+					new InfoColor(Color.rgb(255, 0, 0), Color.rgb(255, 0, 255)));
+			put(R.id.elem3,
+					new InfoColor(Color.rgb(0, 153, 51), Color.rgb(0, 153, 255)));
+			put(R.id.elem4,
+					new InfoColor(Color.rgb(0, 51, 204), Color
+							.rgb(204, 51, 204)));
+			put(R.id.elem5,
+					new InfoColor(Color.rgb(255, 0, 0), Color.rgb(255, 0, 255)));
+			put(R.id.elem6,
+					new InfoColor(Color.rgb(0, 153, 51), Color.rgb(0, 153, 255)));
+			put(R.id.elem7,
+					new InfoColor(Color.rgb(0, 51, 204), Color
+							.rgb(204, 51, 204)));
+			put(R.id.elem8,
+					new InfoColor(Color.rgb(255, 204, 0), Color.rgb(255, 204,
+							204)));
+
+		}
+	};
+
 	private SeekBar seekbar;
-	
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		seekbar = (SeekBar)findViewById(R.id.elemSeekBar);
+
+		seekbar = (SeekBar) findViewById(R.id.elemSeekBar);
+		TextView elem = null;
+
+		for (Integer element : elencoId) {
+
+			transitionColor.get(element).setMaxTransition(seekbar.getMax());
+			elem = (TextView) findViewById(element);
+			elem.setBackgroundColor(transitionColor.get(element)
+					.getStartColor());
+		}
 		seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-			
+
 			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {}
-			
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+
 			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {}
-			
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
 				TextView elem = null;
-				for(Integer id : elencoId){
-					elem = (TextView)findViewById(id);
-					int actualColor = ((ColorDrawable)elem.getBackground()).getColor();
-					elem.setBackgroundColor(actualColor+progress);
+				for (Integer id : elencoId) {
+					elem = (TextView) findViewById(id);
+					elem.setBackgroundColor(transitionColor.get(id)
+							.getNextColorFromRatio(progress));
 				}
 			}
 		});
@@ -64,13 +111,44 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_about) {
-			return true;
+			
+			DialogOpenSite.newInstance().show(getFragmentManager(), "Alert");
+			
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public static class DialogOpenSite extends DialogFragment {
+		
+		public static DialogOpenSite newInstance() {
+			return new DialogOpenSite();
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return new AlertDialog.Builder(getActivity())
+			.setMessage("Inspired by the works of modern artist. Learn More?")
+			.setCancelable(false)
+			.setNegativeButton("Not Now",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,
+							int id) {
+						dismiss();
+					}
+				})
+			.setPositiveButton("Visit MOMA",
+				  new DialogInterface.OnClickListener() {
+					public void onClick(
+							final DialogInterface dialog, int id) {
+						Uri webpage = Uri.parse(URL);
+						
+				        Intent baseIntent = new Intent(Intent.ACTION_VIEW, webpage);
+						
+						startActivity(baseIntent);
+					}
+				}).create();
+		}
 	}
 }
